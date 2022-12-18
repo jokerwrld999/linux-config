8191457e03144f1d126c9230f571c006614d146a
jokerwrld999 <ifalaleev49@gmail.com>
2022-12-18 21:58:28 +0200

    Ultimate Linux Automation Script

--- install-debian.sh
+++ install-debian.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-
-# >>>>> Global Variables
+# ****  Global Variables
 GITFOLDER=$(pwd)
 DOWNLOADS=~/Downloads
 DOTCONFIG=~/.config
 SHARE=~/.local/share
 PACKS="wget gpg gh nano flameshot remmina tldr telegram-desktop latte-dock unzip gimp vim zip tree python3-pip neofetch gparted easyeffects zsh"
-
 
 echo -ne "
 -------------------------------------------------------------------------
@@ -15,15 +13,38 @@ echo -ne "
                Installing Essential Packages 
 -------------------------------------------------------------------------
 "
-sudo apt install -y nala
-sudo nala install -y apt-transport-https
-sudo nala update && sudo nala upgrade -y
-sudo nala install -y $PACKS
-
+if [[ -x "$(command -v apt)" ]]
+then
+   sudo apt install -y nala
+   sudo nala install -y apt-transport-https
+   sudo nala update && sudo nala upgrade -y
+   sudo nala install -y $PACKS
+elif [[ -x "$(command -v dnf)" ]]
+then
+   sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+   sudo dnf install -y https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+   sudo dnf install -y fedora-workstation-repositories
+   # *** Installing Chrome
+   sudo dnf config-manager --set-enabled google-chrome
+   sudo dnf update -y
+   sudo dnf install -y $PACKS
+elif [[ -x "$(command -v pacman)" ]]
+then
+   sudo pacman -Syyu --noconfirm
+   sudo pacman -S --noconfirm --needed base-devel $PACKS
+   
+   # *** Installing Yay AUR Helper
+   sudo git clone https://aur.archlinux.org/yay-git.git /opt/yay-git
+   sudo chown -R $(whoami): /opt/yay-git
+   cd /opt/yay-git/
+   yes | makepkg -si
+else
+   echo "FAILED TO INSTALL PACKAGE: Package manager not found. You must manually install: $PACKS">&2;
+fi
 
 echo -ne "
 -------------------------------------------------------------------------
-                    Copying Backup Data  
+                    Copying Needed Data  
 -------------------------------------------------------------------------
 "
 sudo chmod -R 755 $GITFOLDER
@@ -32,151 +53,205 @@ yes | sudo cp -ri $GITFOLDER/dotconfig/* $DOTCONFIG/
 yes | sudo cp -ri $GITFOLDER/resources/wallpapers  $SHARE/
 yes | sudo cp -ri $GITFOLDER/resources/fonts $SHARE/
 
-# *** Reloading Font
+# **** Reloading Font
 fc-cache -vf
 
-
 echo -ne "
 -------------------------------------------------------------------------
-                    Adding Chrome To The Repository  
+                    Installing Chrome  
 -------------------------------------------------------------------------
 "
-wget -nv https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -P $DOWNLOADS/
-sudo nala install -y $DOWNLOADS/google-chrome-stable_current_amd64.deb
-
+if [[ -x "$(command -v nala)" ]]
+then
+   wget -nv https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -P $DOWNLOADS/
+   sudo nala install -y $DOWNLOADS/google-chrome-stable_current_amd64.deb
+elif [[ -x "$(command -v yay)" ]]
+then
+   # *** Installing Chrome | AUR
+   yes | yay -S google-chrome
+else
+   echo "FAILED TO INSTALL Chrome!"
+fi
 
 echo -ne "
 -------------------------------------------------------------------------
-                    Adding Vs Code Repository  
+                    Installing Vs Code  
 -------------------------------------------------------------------------
 "
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
-rm -f packages.microsoft.gpg
-sudo nala install -y code
-
+if [[ -x "$(command -v nala)" ]]
+then
+   wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+   sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+   sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+   rm -f packages.microsoft.gpg
+   sudo nala install -y code
+elif [[ -x "$(command -v dnf)" ]]
+then
+   sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+   sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+   sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+   sudo dnf install -y code
+elif [[ -x "$(command -v yay)" ]]
+then
+   # *** Installing VS Code | AUR
+   yes | yay -S visual-studio-code-bin
+else
+   echo "FAILED TO INSTALL Code!"
+fi
 
 echo -ne "
 -------------------------------------------------------------------------
-                    Installing Snaps  
+                    Installing Snap Store
 -------------------------------------------------------------------------
-" 
-sudo nala install -y snapd
+"
+if [[ -x "$(command -v nala)" ]]
+then
+   sudo nala install -y snapd
+elif [[ -x "$(command -v dnf)" ]]
+then
+   sudo dnf install snapd
+   sudo ln -s /var/lib/snapd/snap /snap
+elif [[ -x "$(command -v yay)" ]]
+then
+   # *** Installing Snap | AUR
+   yes | yay -S snapd
+   sudo systemctl enable --now snapd.socket
+else
+   echo "FAILED TO INSTALL Snap Store!"
+fi
+
+echo -ne "
+-------------------------------------------------------------------------
+                    Installing Snap Packages
+-------------------------------------------------------------------------
+"
+sudo ln -s /var/lib/snapd/snap /snap
 sudo snap install core
 sudo snap install haruna --candidate
 sudo snap install discord
-
 
 echo -ne "
 -------------------------------------------------------------------------
                     Installing KDE Related Packages  
 -------------------------------------------------------------------------
 "
-sudo nala install -y kde-standard 
-
+if [[ -x "$(command -v nala)" ]]
+then
+   sudo nala install -y kde-standard
+elif [[ -x "$(command -v dnf)" ]]
+then
+   sudo dnf group install -y "KDE Plasma Workspaces"
+elif [[ -x "$(command -v pacman)" ]]
+then
+   sudo pacman -S xorg plasma plasma-wayland-session kde-applications
+   systemctl enable sddm
+   systemctl enable NetworkManager
+else
+   echo "FAILED TO INSTALL KDE!"
+fi
 
 echo -ne "
 -------------------------------------------------------------------------
                     KWin Scripts  
 -------------------------------------------------------------------------
 " 
-# *** Force Blur
+# **** Force Blur
 git clone https://github.com/esjeon/kwin-forceblur.git $DOWNLOADS/kwin-forceblur
 cd $DOWNLOADS/kwin-forceblur/
-sudo ./pack.sh
+./pack.sh
 ./install.sh
 
-# *** Latte Windows
+# *** Enable Script Configuration
+ mkdir -p $SHARE/kservices5/
+ yes | cp -ri $SHARE/kwin/scripts/forceblur/metadata.desktop $SHARE/kservices5/forceblur.desktop
+
+# **** Latte Windows
 git clone https://github.com/psifidotos/kwinscript-window-colors.git $DOWNLOADS/kwinscript-window-colors
 cd $DOWNLOADS/kwinscript-window-colors
 plasmapkg2 -i .
-
 
 echo -ne "
 -------------------------------------------------------------------------
                     Installing Theme, Icons, Fonts, Cursors
                        Configure Kvantum Theme And SDDM  
 -------------------------------------------------------------------------
-" 
-sudo nala install -y gtk2-engines-murrine sassc qt5-style-kvantum qt5-style-kvantum-themes
+"
+if [[ -x "$(command -v nala)" ]]
+then
+   sudo nala install -y gtk2-engines-murrine sassc qt5-style-kvantum qt5-style-kvantum-themes
+elif [[ -x "$(command -v dnf)" ]]
+then
+   sudo dnf install -y gtk-murrine-engine sassc kvantum
+elif [[ -x "$(command -v pacman)" ]]
+then
+   sudo pacman -S --noconfirm gtk-engine-murrine sassc kvantum-qt5
+else
+   echo "FAILED TO INSTALL Theme Packages!"
+fi
 
-# *** SDDM"
+# **** SDDM
 systemctl disable gdm
 systemctl enable sddm
 sudo mkdir -p /etc/sddm.conf.d
 git clone https://github.com/icaho/Swish.git $DOWNLOADS/Swish
 yes | sudo cp -ri $DOWNLOADS/Swish/ /usr/share/sddm/themes/
 yes | sudo cp -ri $GITFOLDER/resources/sddm/kde_settings.conf /etc/sddm.conf.d/
-
-                
-# *** Splash Screen
+            
+# **** Splash Screen
 plasmapkg2 -i $GITFOLDER/resources/splash-screen/watch-dogs-splash.plasmoid
 yes | sudo cp -ri $GITFOLDER/resources/splash-screen/ksplashrc $DOTCONFIG/
 
-
-# *** Systray Latte Tweaks
+# **** Systray Latte Tweaks
 git clone https://github.com/psifidotos/plasma-systray-latte-tweaks.git $DOWNLOADS/plasma-systray-latte-tweaks
 cd $DOWNLOADS/plasma-systray-latte-tweaks/
 mkdir -p $SHARE/plasma/plasmoids/
 yes | sudo cp -ri org.kde.plasma.systemtray org.kde.plasma.private.systemtray $SHARE/plasma/plasmoids/
 
-
-# *** Orchis KDE"
+# **** Orchis KDE
 git clone https://github.com/vinceliuice/Orchis-kde.git $DOWNLOADS/Orchis-kde
 cd $DOWNLOADS/Orchis-kde/
 ./install.sh
 
-
-# *** Orchis KDE Theme"
+# **** Orchis KDE Theme
 git clone https://github.com/vinceliuice/Orchis-theme.git $DOWNLOADS/Orchis-theme
 cd $DOWNLOADS/Orchis-theme/
 ./install.sh -t purple --tweaks black
 
-
-# *** Sevi Icon Theme
+# **** Sevi Icon Theme
 git clone https://github.com/TaylanTatli/Sevi.git $DOWNLOADS/Sevi
 cd $DOWNLOADS/Sevi/
 ./install.sh -black
 /usr/lib/x86_64-linux-gnu/libexec/plasma-changeicons Sevi-black
 
-
-# *** Bibata Cursors
+# **** Bibata Cursors
 tar -xvf $GITFOLDER/resources/cursors/Bibata-*.tar.gz --directory $DOWNLOADS
 yes | sudo cp -ri $DOWNLOADS/Bibata-* ~/.icons/
 yes | sudo cp -ri $DOWNLOADS/Bibata-* /usr/share/icons/
-
 
 echo -ne "
 -------------------------------------------------------------------------
                     Install Plasmoids  
 -------------------------------------------------------------------------
 " 
-# *** Plasma Customization Saver
+# **** Plasma Customization Saver
 git clone https://github.com/paju1986/PlasmaConfSaver.git $DOWNLOADS/PlasmaConfSaver
 cd $DOWNLOADS/PlasmaConfSaver/com.pajuelo.plasmaConfSaver/
 plasmapkg2 -i .
 
-# *** Latte Spacer
+# **** Latte Spacer
 git clone https://github.com/psifidotos/applet-latte-spacer.git $DOWNLOADS/applet-latte-spacer
 cd $DOWNLOADS/applet-latte-spacer/
 plasmapkg2 -i .
 
-# *** Latte Separator
+# **** Latte Separator
 git clone https://github.com/psifidotos/applet-latte-separator.git $DOWNLOADS/applet-latte-separator
 cd $DOWNLOADS/applet-latte-separator/
 plasmapkg2 -i .
 
-# *** Ditto Menu
-git clone https://github.com/adhec/dittoMenuKDE.git $DOWNLOADS/dittoMenuKDE
-cd $DOWNLOADS/dittoMenuKDE/package/
-plasmapkg2 -i .
-
-# *** Shutdown Switch
+# **** Shutdown Switch
 git clone https://github.com/Davide-sd/shutdown_or_switch.git $DOWNLOADS/shutdown_or_switch
 cd $DOWNLOADS/shutdown_or_switch/plasmoid/
 plasmapkg2 -i .
-
 
 echo -ne "
 -------------------------------------------------------------------------
@@ -185,54 +260,87 @@ echo -ne "
 " 
 git clone https://github.com/wsdfhjxc/virtual-desktop-bar.git $DOWNLOADS/virtual-desktop-bar
 cd $DOWNLOADS/virtual-desktop-bar/
-yes | sudo ./scripts/install-dependencies-ubuntu.sh
+ 
+if [[ -x "$(command -v nala)" ]]
+then
+   yes | sudo ./scripts/install-dependencies-ubuntu.sh
+elif [[ -x "$(command -v dnf)" ]]
+then
+   yes | sudo ./scripts/install-dependencies-fedora.sh
+elif [[ -x "$(command -v pacman)" ]]
+then
+   yes | sudo ./scripts/install-dependencies-arch.sh
+else
+   echo "FAILED TO INSTALL Desktop Bar!"
+fi
 ./scripts/install-applet.sh
-
 
 echo -ne "
 -------------------------------------------------------------------------
                     Install And Configure ZSH 
 -------------------------------------------------------------------------
 " 
-# *** Auto-Suggestions Plugin
+# **** Auto-Suggestions Plugin
 mkdir -p ~/.zsh
-sudo nala install -y autojump
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
+# **** POWERLEVEL10K
+git clone https://github.com/romkatv/powerlevel10k.git ~/.zsh//powerlevel10k
 
-# *** Copying Configs
+if [[ -x "$(command -v nala)" ]]
+then
+   sudo nala install -y autojump
+elif [[ -x "$(command -v dnf)" ]]
+then
+   sudo dnf install -y autojump
+elif [[ -x "$(command -v yay)" ]]
+then
+   # *** Auto-Jump Plugin | AUR
+   yes | yay -S autojump
+else
+   echo "FAILED TO INSTALL ZSH Packages!"
+fi
+
+# **** Copying Configs
 touch "$HOME/.cache/zshhistory"
 yes | cp -ri $GITFOLDER/resources/zsh/.zshrc ~/
 yes | cp -ri $GITFOLDER/resources/zsh/aliasrc ~/.zsh/
 yes | cp -ri $GITFOLDER/resources/zsh/.p10k.zsh ~/.zsh/
 
-# *** POWERLEVEL10K
-git clone https://github.com/romkatv/powerlevel10k.git ~/.zsh//powerlevel10k
-
-
 echo -ne "
 -------------------------------------------------------------------------
-                    Konsave Plasma Saver 
--------------------------------------------------------------------------
-" 
-python -m pip install konsave
-#konsave -i $GITFOLDER/resources/konsave/JokerWrld-Theme.knsv
-#konsave -a JokerWrld-Theme.knsv
-
-
-echo -ne "
--------------------------------------------------------------------------
-                    Cleaning Up The Downloads Folder 
+                    Cleaning Up Temporary Files 
 -------------------------------------------------------------------------
 "
 sudo chmod -R 755 $DOTCONFIG $SHARE
 sudo chown -R $USER:$USER $DOTCONFIG $SHARE
-cd
-rm -rf $DOWNLOADS/*
-sudo apt-get autoclean && sudo apt-get autoremove
+cd && rm -rf $DOWNLOADS/*
 
+if [[ -x "$(command -v apt)" ]]
+then
+   sudo apt autoclean && sudo apt autoremove
+elif [[ -x "$(command -v dnf)" ]]
+then
+   sudo dnf clean all
+elif [[ -x "$(command -v pacman)" ]]
+then
+   sudo pacman -Scc
+   sudo pacman -Rns $(pacman -Qtdq)
+else
+   echo "FAILED TO INSTALL Chrome!"
+fi
+rm -rf ~/.cache/*
 
-# >>>>> Enabling Services and Graphical User Interface
-chsh -s $(which zsh)
+echo -ne "
+-------------------------------------------------------------------------
+                    Changing Shell To ZSH 
+-------------------------------------------------------------------------
+"
+sudo chsh -s $(which zsh) $USER
 
+echo -ne "
+-------------------------------------------------------------------------
+                    Rebooting 
+-------------------------------------------------------------------------
+"
 sudo reboot
